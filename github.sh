d0a7150a7e661d32ff305a7cbf3edf50eb325f7d
joelotz <joelotz+github@gmail.com>
2013-02-22 21:54:31 -0800

    "this is a test commit"

--- github.sh
+++ github.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd ~/Python/github/Projects/URL_Blacklist
+echo -e "Enter commit message: \c "
+read  word
 
 git add .
-git commit -m "my message here"
+git commit -m "\"$word\""
 git push
 
