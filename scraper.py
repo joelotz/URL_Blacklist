e431ee984f0bf8cb2b89190a7cead783f3acc4ff
joelotz <joelotz+github@gmail.com>
2013-02-25 21:26:42 -0800

    buiding skeleton of functions

--- scraper.py
+++ scraper.py
@@ -6,6 +6,9 @@ Date       : 2013-2-25
 Author     : Joe Lotz <joelotz@gmail.com>
 -------------------------------------------------------------------------------
 '''
+import requests
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 
 def scrape_site():
     return
@@ -15,4 +18,11 @@ def clean_links():
     
 def upload_db():
     return
+    
+url = "http://www.xvideos.com"
+r = requests.get(url)
+soup = BeautifulSoup(r.text,"html.parser",parse_only=SoupStrainer("a"))
+for link in soup:
+    print(link.get('href'))
+    
     
